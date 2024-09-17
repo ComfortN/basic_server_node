ef7874c936cad4f4b6847599831ac7a5aeb36763
Comfort Ngwenya <>
2024-09-17 10:17:13 +0200

    Adding a GET method and its responses

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const PORT = 8887;
 const localhost = 'localhost';
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    console.log(`Received ${req.method} request for ${req.url}`);
+    
+    if (req.url === '/') {
+        if (req.method === 'GET') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Welcome to the Basic Node.js Server!');
+        }  else {
+            res.writeHead(405, { 'Content-Type': 'text/plain' });
+            res.end('Method Not Allowed');
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
 });
 
-
-server.listen(PORT, localhost, () => {
-    console.log(`Server running at: ${localhost} : ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server running at http://${localhost}:${PORT}/`);
 });
+
